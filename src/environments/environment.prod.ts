6548ea06c41351506e60d51b084fada70afe063d
Jani Andsten <jani.andsten@gmail.com>
2023-03-04 13:10:43 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '3fa16246d70211b92d1c182141c872fa',
+  apiUrl: 'https://api.themoviedb.org/3',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
